aca48bd24de45e7dff944ac076fda1f7b6399edd
Nikesh Bhagalia <nikeshbhagalia@hotmail.com>
2021-03-17 17:26:00 +1300

    still trying, might have to use selenium

---
+++ Youtube Thumbnail Getter/Program.cs
@@ -0,0 +1,37 @@
+﻿using HtmlAgilityPack;
+using OpenQA.Selenium;
+using System;
+using System.Threading.Tasks;
+
+namespace Youtube_Thumbnail_Getter
+{
+    class Program
+    {
+        private const string BaseUrl = @"https://www.youtube.com/c/ChannelNameOrId/videos";
+        private const string ChannelNameOrId = "";
+        private const string ThumbnailImgTagXpath = "//img[contains(@src, 'hqdefault')]";
+
+        static async Task Main(string[] args)
+        {
+            //var htmlWeb = new HtmlWeb();
+            //var document = htmlWeb.Load(BaseUrl.Replace(nameof(ChannelNameOrId), ChannelNameOrId));
+            //var tableRows = document.DocumentNode.SelectNodes(ThumbnailImgTagXpath);
+            Console.WriteLine("Hello World!");
+
+            IWebDriver driver = new PhantomJSDriver();
+            driver.Navigate().GoToUrl("http://www.google.com/");
+
+
+            IWebElement query = driver.FindElement(By.Name("q"));
+            query.SendKeys("Cheese");
+            query.Submit();
+
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            wait.Until((d) => { return d.Title.ToLower().StartsWith("cheese"); });
+
+            System.Console.WriteLine("Page title is: " + driver.Title);
+
+            driver.Quit();
+        }
+    }
+}
